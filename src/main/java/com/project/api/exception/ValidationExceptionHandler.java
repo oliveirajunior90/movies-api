e0c7f3c7fd9a02e8ee4b241ff7e23ba9ed3bcc0b
oliveirajunior90 <oliveira_junior90@hotmail.com>
2025-03-21 18:01:32 -0300

    remove error dto

--- src/main/java/com/project/api/exception/ValidationExceptionHandler.java
+++ src/main/java/com/project/api/exception/ValidationExceptionHandler.java
@@ -1,9 +1,5 @@
 package com.project.api.exception;
 
-import com.project.api.dto.ErrorResponseDTO;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
